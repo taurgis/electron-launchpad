ecc9e791c0db1caf22396cebdef3fbd531f2f3f2
Thomas Theunen <thomas.theunen@fenego.be>
2017-07-14 08:31:02 +0200

    Update for codeclimate

--- src/main/config/menu/file.js
+++ src/main/config/menu/file.js
@@ -2,37 +2,35 @@ import {
   getWindowManager
 } from '../../lib/window-manager';
 import {
-  openFile,
-  openFileForImporting,
-  newFile
+  openFile, openFileForImporting, newFile
 } from '../../lib/files';
 
 const windowManager = getWindowManager();
 
 export default [{
-    label: 'Open Log File',
-    accelerator: 'CmdOrCtrl+O',
-    click: (item, focusedWindow) => openFile(focusedWindow)
-  },
-  {
-    type: 'separator'
-  },
-  {
-    type: 'separator'
-  },
-  {
-    label: 'Open New Window',
-    accelerator: 'CmdOrCtrl+Shift+N',
-    click: () => {
-      windowManager.buildWindowOfType('main');
-    }
-  },
-  {
-    type: 'separator'
-  },
-  {
-    label: 'Close Window',
-    accelerator: 'CmdOrCtrl+W',
-    role: 'close'
+  label: 'Open Log File',
+  accelerator: 'CmdOrCtrl+O',
+  click: (item, focusedWindow) => openFile(focusedWindow)
+},
+{
+  type: 'separator'
+},
+{
+  type: 'separator'
+},
+{
+  label: 'Open New Window',
+  accelerator: 'CmdOrCtrl+Shift+N',
+  click: () => {
+    windowManager.buildWindowOfType('main');
   }
+},
+{
+  type: 'separator'
+},
+{
+  label: 'Close Window',
+  accelerator: 'CmdOrCtrl+W',
+  role: 'close'
+}
 ];
